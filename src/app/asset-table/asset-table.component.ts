b10a2703b9ec47701e5955d3293852dc7cf5f0e0
Ryan Beckett <beckett.ryan@gmail.com>
2018-04-11 19:41:13 -0400

    - Adding back unindexed files to Git.

---
+++ src/app/asset-table/asset-table.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {AssetService} from "../asset/asset.service";
+import {ActivatedRoute, Router} from "@angular/router";
+
+
+@Component({
+  selector: 'app-asset-table',
+  templateUrl: './asset-table.component.html',
+  styleUrls: ['./asset-table.component.css']
+})
+export class AssetTableComponent implements OnInit {
+
+  assets: Array<any>;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private assetService: AssetService) {
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.assetService.getAssetByQueryParams(params).subscribe(data => {
+        this.assets = data;
+      });
+    });
+  }
+
+}
